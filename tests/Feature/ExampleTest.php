0eb31e2536a0113dd1d1c7f63770da03bb956172
albertpratomo <albertpratomo21@gmail.com>
2021-01-31 14:19:38 +0700

    Install pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,20 +1,5 @@
 <?php
 
-namespace Tests\Feature;
-
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+test('example test', function () {
+    expect(true)->toBeTrue();
+});
