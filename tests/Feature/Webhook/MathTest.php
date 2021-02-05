ce563a45f9bc563bc5a2bf613a508bf34393c971
adietyachristian <adietya.christian@ui.ac.id>
2021-02-05 15:16:13 +0700

    Additional Math Test Cases

--- tests/Feature/Webhook/MathTest.php
+++ tests/Feature/Webhook/MathTest.php
@@ -4,6 +4,22 @@ test('bot can calculate 1+1', function () {
     $this->receiveMessage('1+1');
 
     $this->assertRequestSent();
+});
 
-    // delete me
+test('bot can calculate 272  -  53', function () {
+    $this->receiveMessage('272  -  53');
+
+    $this->assertRequestSent();
+});
+
+test('bot can calculate  4 * 25', function () {
+    $this->receiveMessage(' 4 * 25');
+
+    $this->assertRequestSent();
+});
+
+test('bot can calculate 64 /8', function () {
+    $this->receiveMessage('64 /8');
+
+    $this->assertRequestSent();
 });
