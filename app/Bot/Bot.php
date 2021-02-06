e8ef2a309192511a5d29243a820c3d70d51b4344
albertpratomo <albertpratomo21@gmail.com>
2021-02-06 20:37:45 +0700

    Improve bot telling time

--- app/Bot/Bot.php
+++ app/Bot/Bot.php
@@ -2,8 +2,8 @@
 
 namespace App\Bot;
 
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class Bot
 {
@@ -19,83 +19,76 @@ class Bot
         // Define which operation is called
         // Assign reply with the result
 
-        if (strpos($message, '+')) {
-            $message = preg_replace('/\s+/', '', $message);
-            echo "tambah\n";
-            $pieces = explode('+', $message);
-            $reply = (int) $pieces[0] + (int) $pieces[1];
-        } elseif (strpos($message, '-')) {
-            $message = preg_replace('/\s+/', '', $message);
-            echo "kurang\n";
-            $pieces = explode('-', $message);
-            $reply = $pieces[0] - $pieces[1];
-        } elseif (strpos($message, '*') or strpos($message, 'x')) {
-            $message = preg_replace('/\s+/', '', $message);
-            echo "kali\n";
-            if (strpos($message, '*')) {
-                $pieces = explode('*', $message);
-            } elseif (strpos($message, 'x')) {
-                $message = preg_replace('/\s+/', '', $message);
-                $pieces = explode('x', $message);
-            }
-            $reply = $pieces[0] * $pieces[1];
-        } elseif (strpos($message, '/') or strpos($message, '÷') or strpos($message, ':')) {
-            $message = preg_replace('/\s+/', '', $message);
-            echo "bagi\n";
-            if (strpos($message, '/')) {
-                $pieces = explode('/', $message);
-            } elseif (strpos($message, '÷')) {
-                $message = preg_replace('/\s+/', '', $message);
-                $pieces = explode('÷', $message);
-            } elseif (strpos($message, ':')) {
-                $message = preg_replace('/\s+/', '', $message);
-                $pieces = explode(':', $message);
-            }
-            $reply = $pieces[0] / $pieces[1];
-        } elseif ($message === 'jam') {
-            // $current = Carbon::now()->format('H:i');
-            $current = Carbon::now()->setTimezone('Asia/Bangkok')->format('H:i');
-            // $current->setTimezone('Asia/Bangkok');
-            $reply = $current;
-        } elseif ($message === 'hari') {
-            $current = Carbon::now()->setTimezone('Asia/Bangkok')->format('l');
-            $reply = $current;
-        } elseif ($message === 'hari jam') {
-            $current = Carbon::now()->setTimezone('Asia/Bangkok')->format('l H:i');
-            $reply = $current;
-        } elseif ($message === 'hari indo') {
-            $current = Carbon::now()->setTimezone('Asia/Bangkok')->format('l');
-            switch ($current) {
-                case 'Monday':
-                    $day = 'Senin';
-                break;
-                case 'Tuesday':
-                    $day = 'Selasa';
-                break;
-                case 'Wednesday':
-                    $day = 'Rabu';
-                break;
-                case 'Thursday':
-                    $day = 'Kamis';
-                break;
-                case 'Friday':
-                    $day = 'Jumat';
-                break;
-                case 'Saturday':
-                    $day = 'Sabtu';
-                break;
-                case 'Sunday':
-                    $day = 'Minggu';
-                break;
-                default:
-                $day = 'Error';
-            }
-            $reply = $day;
+        if (static::isMathExpression($message)) {
+            $reply = static::calculateMathExpression($message);
+        } elseif (static::isAskingTime($message)) {
+            $reply = static::tellTime($message);
         } else {
             $reply = "I'm still learning, so I don't understand '$message' yet. Chat with me again in a few days!";
         }
 
         static::sendMessage($reply, $event['sender']['id']);
+    }
+
+    public static function isMathExpression(string $message): bool
+    {
+        return Str::contains($message, ['+', '-', '*', '/', 'x', ':', '÷']);
+    }
+
+    public static function calculateMathExpression(string $message): string
+    {
+        // Trim whitespace.
+        $message = preg_replace('/\s+/', '', $message);
+
+        // Extract the operator out of the message.
+        foreach (['+', '-', '*', '/', 'x', ':', '÷'] as $sign) {
+            if (Str::contains($message, $sign)) {
+                $operator = $sign;
+
+                break;
+            }
+        }
+
+        // Find the 2 numbers to calculate.
+        $pieces = explode($operator, $message);
+
+        // Calculate according to the operator.
+        switch ($operator) {
+            case '+':
+                return $pieces[0] + $pieces[1];
+
+            case '-':
+                return $pieces[0] - $pieces[1];
+
+            case '*':
+            case 'x':
+                return $pieces[0] * $pieces[1];
+
+            case '/':
+            case ':':
+            case '÷':
+                return $pieces[0] / $pieces[1];
+        }
+    }
+
+    public static function isAskingTime(string $message): bool
+    {
+        return Str::contains($message, ['hari', 'jam']);
+    }
+
+    public static function tellTime(string $message): string
+    {
+        $reply = '';
+
+        if (Str::contains($message, 'hari')) {
+            $reply .= now()->format('l ');
+        }
+
+        if (Str::contains($message, 'jam')) {
+            $reply .= now()->format('H:i');
+        }
+
+        return $reply;
     }
 
     /**
