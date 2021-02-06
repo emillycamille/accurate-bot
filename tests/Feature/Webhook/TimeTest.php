bf84e80d6548d156d6172e6569830c2815b98562
albertpratomo <albertpratomo21@gmail.com>
2021-02-06 19:16:48 +0700

    Fix time test

--- tests/Feature/Webhook/TimeTest.php
+++ tests/Feature/Webhook/TimeTest.php
@@ -1,20 +1,31 @@
 <?php
 
+use Carbon\Carbon;
+
+beforeEach(function () {
+    $time = new Carbon('06-02-2021 10:00');
+
+    $this->travelTo($time);
+});
+
 test('bot can tell time', function () {
     $this->receiveMessage('jam');
 
     $this->assertRequestSent();
 });
+
 test('bot can tell day', function () {
     $this->receiveMessage('hari');
 
     $this->assertRequestSent();
 });
+
 test('bot can tell day in indonesian', function () {
     $this->receiveMessage('hari indo');
 
     $this->assertRequestSent();
 });
+
 test('bot can tell day and time', function () {
     $this->receiveMessage('hari jam');
 
