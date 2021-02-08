f8067aa28584220dc0d1d00930074c1ba847d631
adietyachristian <adietya.christian@ui.ac.id>
2021-02-08 16:22:02 +0700

    Bot greet and weather

--- app/Bot/Bot.php
+++ app/Bot/Bot.php
@@ -5,11 +5,12 @@ namespace App\Bot;
 use App\Bot\Traits\CanDoMath;
 use App\Bot\Traits\CanTellTime;
 use App\Bot\Traits\CanGreetUser;
+use App\Bot\Traits\CanTellWeather;
 use Illuminate\Support\Facades\Http;
 
 class Bot
 {
-    use CanDoMath, CanTellTime, CanGreetUser;
+    use CanDoMath, CanTellTime, CanTellWeather, CanGreetUser;
 
     /**
      * Handle received message event.
@@ -24,6 +25,8 @@ class Bot
             $reply = static::calculateMathExpression($message);
         } elseif (static::isAskingTime($message)) {
             $reply = static::tellTime($message);
+        } elseif (static::isAskingWeather($message)) {
+            $reply = static::tellWeather($message);
         } elseif (static::isSayingHello($message)) {
             $reply = static::greetUser($message);
         } else {
