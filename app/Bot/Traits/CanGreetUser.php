0b89129f819961dc23d8cfed3188079985cb33c1
adietyachristian <adietya.christian@ui.ac.id>
2021-02-08 16:47:11 +0700

    Can Greet User id Only

--- app/Bot/Traits/CanGreetUser.php
+++ app/Bot/Traits/CanGreetUser.php
@@ -20,11 +20,11 @@ trait CanGreetUser
     public static function greetUser(string $message, $userID): string
     {
         // $fbPageToken = env('FB_PAGE_TOKEN');
-        $json = json_decode(file_get_contents("https://graph.facebook.com/v3.2/{$userID}?access_token=EAACVBIrdOPMBAATZBa4bqr5ZBWIFUecMGnRqw4E5usQlPXQdjZCfkt2ZAkEZALx8RtabnG6BDajKHYh2CcvOhZBxLQoM67ZB1ZB3U2nmATIPeVOOKJgK4qVP5Cjv6TUpLtHKtB8tWW5MaI53QK2c3NWKnuhdjWLYPmyrNDZCOycbvnuRB3ZC1PHHOk"), true);
-        $name = $json['name'];
-        $nameSplit = preg_split('/\s+/',$name);
-        $nickname = $nameSplit[0];
-        return "Halo {$nickname}!";
+        // $json = json_decode(file_get_contents("https://graph.facebook.com/v3.2/{$userID}?access_token=EAACVBIrdOPMBAATZBa4bqr5ZBWIFUecMGnRqw4E5usQlPXQdjZCfkt2ZAkEZALx8RtabnG6BDajKHYh2CcvOhZBxLQoM67ZB1ZB3U2nmATIPeVOOKJgK4qVP5Cjv6TUpLtHKtB8tWW5MaI53QK2c3NWKnuhdjWLYPmyrNDZCOycbvnuRB3ZC1PHHOk"), true);
+        // $name = $json['name'];
+        // $nameSplit = preg_split('/\s+/',$name);
+        // $nickname = $nameSplit[0];
+        return "Halo {$userID}!";
 
     }
 }
