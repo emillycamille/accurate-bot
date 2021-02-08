6e6c6240fd29be974f7d11da7cf5aeeaf536daac
adietyachristian <adietya.christian@ui.ac.id>
2021-02-08 16:38:50 +0700

    CanGreetUser Test 1

--- app/Bot/Traits/CanGreetUser.php
+++ app/Bot/Traits/CanGreetUser.php
@@ -17,10 +17,10 @@ trait CanGreetUser
     /**
      * Tell the current time, as requested in $message.
      */
-    public static function greetUser(string $message): string
+    public static function greetUser(string $message, $userID): string
     {
         // $fbPageToken = env('FB_PAGE_TOKEN');
-        $json = json_decode(file_get_contents("https://graph.facebook.com/v3.2/100000188118411?access_token=EAACVBIrdOPMBAATZBa4bqr5ZBWIFUecMGnRqw4E5usQlPXQdjZCfkt2ZAkEZALx8RtabnG6BDajKHYh2CcvOhZBxLQoM67ZB1ZB3U2nmATIPeVOOKJgK4qVP5Cjv6TUpLtHKtB8tWW5MaI53QK2c3NWKnuhdjWLYPmyrNDZCOycbvnuRB3ZC1PHHOk"), true);
+        $json = json_decode(file_get_contents("https://graph.facebook.com/v3.2/{$userID}?access_token=EAACVBIrdOPMBAATZBa4bqr5ZBWIFUecMGnRqw4E5usQlPXQdjZCfkt2ZAkEZALx8RtabnG6BDajKHYh2CcvOhZBxLQoM67ZB1ZB3U2nmATIPeVOOKJgK4qVP5Cjv6TUpLtHKtB8tWW5MaI53QK2c3NWKnuhdjWLYPmyrNDZCOycbvnuRB3ZC1PHHOk"), true);
         $name = $json['name'];
         $nameSplit = preg_split('/\s+/',$name);
         $nickname = $nameSplit[0];
