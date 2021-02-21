a913a622bacc8e0ec1b2260c446555ce520ea7a5
albertpratomo <albertpratomo21@gmail.com>
2021-02-21 16:50:50 +0700

    Refactor code

--- app/Bot/Traits/CanGreetUser.php
+++ app/Bot/Traits/CanGreetUser.php
@@ -20,11 +20,8 @@ trait CanGreetUser
      */
     public static function greetUser(string $message, string $userID): string
     {
-
-        // Should use Http::get.
-        $response = Http::get(config('bot.fb_user_url').$userID, [
+        $response = Http::get(config('bot.fb_api_url').$userID, [
             'access_token' => config('bot.fb_page_token'),
-
         ]);
 
         $name = $response['first_name'];
