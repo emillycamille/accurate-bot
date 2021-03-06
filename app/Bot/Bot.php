0c3dc5f504e49038faaf6edcb19022caef533f0a
adietyachristian <adietya.christian@ui.ac.id>
2021-03-06 23:10:19 +0700

    Update Bot

--- app/Bot/Bot.php
+++ app/Bot/Bot.php
@@ -2,6 +2,8 @@
 
 namespace App\Bot;
 
+use App\Bot\Traits\Accurate\CanShowPurchase;
+use App\Bot\Traits\Accurate\CanShowSales;
 use App\Bot\Traits\CanConnectAccurate;
 use App\Bot\Traits\CanDoMath;
 use App\Bot\Traits\CanGreetUser;
@@ -13,7 +15,7 @@ use Illuminate\Support\Str;
 
 class Bot
 {
-    use CanDoMath, CanTellTime, CanTellWeather, CanGreetUser, CanConnectAccurate;
+    use CanDoMath, CanTellTime, CanTellWeather, CanGreetUser, CanConnectAccurate, CanShowPurchase, CanShowSales;
 
     /**
      * Get the handler method (camelCase string) and payload of $postback event.
@@ -110,6 +112,10 @@ class Bot
             $reply = static::calculateMathExpression($message);
         } elseif (static::isSayingHello($message)) {
             $reply = static::greetUser($message, $senderId);
+        } elseif (static::isAskingPurchaseInvoice($message)) {
+            static::purchaseInvoice($senderId);
+        } elseif (static::isAskingSalesInvoice($message)) {
+            static::salesInvoice($senderId);
         } else {
             $reply = "I'm still learning, so I don't understand '$message' yet. Chat with me again in a few days!";
         }
