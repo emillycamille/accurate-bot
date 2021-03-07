48aa09db0862111aa18f23046ea6c45005cf46d8
adietyachristian <adietya.christian@ui.ac.id>
2021-03-07 16:42:19 +0700

    Add CanGreet

--- app/Bot/Traits/Accurate/CanShowPurchase.php
+++ app/Bot/Traits/Accurate/CanShowPurchase.php
@@ -3,9 +3,11 @@
 namespace App\Bot\Traits\Accurate;
 
 use Illuminate\Support\Str;
+use App\Bot\Traits\CanGreetUser;
 
 trait CanShowPurchase
 {
+    use CanGreetUser;
     /**
      * Determines whether the user is asking about purchase invoice. If yes,
      * return the keyword of the item being asked.
