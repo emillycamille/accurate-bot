dbf298461a38b887d7e4f9bf82f716bfb1ca5ad3
CarlsonIvanic <carlsonivanicjansen@gmail.com>
2021-03-09 23:50:32 +0700

    Remove comment on HelpTest

--- tests/Feature/Webhook/HelpTest.php
+++ tests/Feature/Webhook/HelpTest.php
@@ -1,10 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Http;
 
 test('bot can show help', function () {
     Http::fake();
     $this->receiveMessage('help');
 
-    // Assert that correct Send API request is sent.
     $this->assertRequestSent();
 });
