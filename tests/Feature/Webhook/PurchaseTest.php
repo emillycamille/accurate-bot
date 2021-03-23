1e3c06ba527f0a3ab4d43a3ffa062955a4867ddb
adietyachristian <adietya.christian@ui.ac.id>
2021-03-23 18:54:15 +0700

    Merge sales date and purchase date to sales n purc

--- tests/Feature/Webhook/PurchaseTest.php
+++ tests/Feature/Webhook/PurchaseTest.php
@@ -6,31 +6,31 @@ use Illuminate\Support\Facades\Http;
 const PURCHASE_ITEMS =
     ['s' => true,
     'd' => [
-            ['totalAmount' => 2059200000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE1',
             'statusName' => 'TEST_STATUS1',
             'vendor'=> [
                 'name' => 'TEST_NAME1',
             ], ],
-            ['totalAmount' => 15999000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE2',
             'statusName' => 'TEST_STATUS2',
             'vendor'=> [
                 'name' => 'TEST_NAME2',
             ], ],
-            ['totalAmount' => 215000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE3',
             'statusName' => 'TEST_STATUS3',
             'vendor'=> [
                 'name' => 'TEST_NAME3',
             ], ],
-            ['totalAmount' => 35499000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE4',
             'statusName' => 'TEST_STATUS4',
             'vendor'=> [
                 'name' => 'TEST_NAME4',
             ], ],
-            ['totalAmount' => 11799000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE5',
             'statusName' => 'TEST_STATUS5',
             'vendor'=> [
@@ -68,6 +68,28 @@ test('bot can show purchase invoice (less than 5 invoices)', function () {
     ]);
 
     $this->receiveMessage('purchase masa lalu');
+
+    $this->assertRequestSent();
+});
+
+test('bot can show purchase invoice at selected date', function () {
+    Http::fake([
+        'purchase-invoice/list.do*' => Http::response(['s' => true, 'd'=>PURCHASE_ITEMS['d'], 'sp' => ['pageCount'=>1]]),
+        '*' => Http::response(),
+    ]);
+
+    $this->receiveMessage('pembelian 3/7/2021 adalah');
+
+    $this->assertRequestSent();
+});
+
+test('bot can\'t show purchase invoice at selected date', function () {
+    Http::fake([
+        'purchase-invoice/list.do*' => Http::response(['d' => []]),
+        '*' => Http::response(),
+    ]);
+
+    $this->receiveMessage('pembelian 3/7/2021');
 
     $this->assertRequestSent();
 });
--- tests/Feature/Webhook/SalesTest.php
+++ tests/Feature/Webhook/SalesTest.php
@@ -6,31 +6,31 @@ use Illuminate\Support\Facades\Http;
 const SALES_ITEMS =
 ['s' => true,
     'd' => [
-            ['totalAmount' => 2059200000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE1',
             'statusName' => 'TEST_STATUS1',
             'customer'=> [
                 'name' => 'TEST_NAME1',
             ], ],
-            ['totalAmount' => 15999000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE2',
             'statusName' => 'TEST_STATUS2',
             'customer'=> [
                 'name' => 'TEST_NAME2',
             ], ],
-            ['totalAmount' => 215000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE3',
             'statusName' => 'TEST_STATUS3',
             'customer'=> [
                 'name' => 'TEST_NAME3',
             ], ],
-            ['totalAmount' => 35499000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE4',
             'statusName' => 'TEST_STATUS4',
             'customer'=> [
                 'name' => 'TEST_NAME4',
             ], ],
-            ['totalAmount' => 11799000,
+            ['totalAmount' => 1,
             'transDate' => 'TEST_DATE5',
             'statusName' => 'TEST_STATUS5',
             'customer'=> [
@@ -68,6 +68,28 @@ test('bot can show sales invoice (less than 5 invoices)', function () {
     ]);
 
     $this->receiveMessage('sales masa lalu');
+
+    $this->assertRequestSent();
+});
+
+test('bot can show sales invoice at selected date', function () {
+    Http::fake([
+        'sales-invoice/list.do*' => Http::response(['s' => true, 'd'=>SALES_ITEMS['d'], 'sp'=>['pageCount'=>1]]),
+        '*' => Http::response(),
+    ]);
+
+    $this->receiveMessage('penjualan 3/7/2021');
+
+    $this->assertRequestSent();
+});
+
+test('bot can\'t show sales invoice at selected date', function () {
+    Http::fake([
+        'sales-invoice/list.do*' => Http::response(['d' => []]),
+        '*' => Http::response(),
+    ]);
+
+    $this->receiveMessage('penjualan 3/7/2021');
 
     $this->assertRequestSent();
 });
