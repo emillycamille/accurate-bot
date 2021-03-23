b2185db9a0765b13401bbee76a7ca891dc6abea4
CarlsonIvanic <carlsonivanicjansen@gmail.com>
2021-03-24 00:23:04 +0700

    Add a comment

--- app/Bot/Traits/Accurate/CanManageSales.php
+++ app/Bot/Traits/Accurate/CanManageSales.php
@@ -72,7 +72,7 @@ trait CanManageSales
         $message .= sprintf(__('bot.page'), compact('page'));
         static::sendMessage($message, $psid);
 
-        // CHANGE: Kasi comment ya.
+        // Send a "Ya" button if the next page is still available.
         if ($items['sp']['pageCount'] > (int) $page) {
             $page += 1;
             $payload = static::makeButtonPayload(__('bot.ask_next_page'), [[
@@ -95,7 +95,7 @@ trait CanManageSales
                 'fields' => 'totalAmount',
                 'filter.dueDate.val' => $date,
                 'page' => $page,
-                ]);
+            ]);
 
             if (count($items['d']) == 0) {
                 static::sendMessage(__('bot.no_sales_at', compact('date')), $psid);
