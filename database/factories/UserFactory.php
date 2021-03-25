4ae4ed03428e5628a6c80cdec390bed0a0519c2e
CarlsonIvanic <carlsonivanicjansen@gmail.com>
2021-03-25 20:49:17 +0700

    Remove inactive code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -42,14 +42,4 @@ class UserFactory extends Factory
             ];
         });
     }
-
-    // public function withFbName(): Factory
-    // {
-    //     return $this->state(function (array $attributes) {
-    //         return [
-    //             'first_name' => 'TEST_FIRST_NAME',
-    //             'last_name' => 'TEST_LAST_NAME',
-    //         ];
-    //     });
-    // }
 }
