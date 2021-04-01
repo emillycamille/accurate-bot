e7b8f30e1b0178b4b1cc886b09f87b3a153465f9
CarlsonIvanic <carlsonivanicjansen@gmail.com>
2021-04-01 15:15:18 +0700

    Create model Reminder

---
+++ database/factories/ReminderFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Reminder;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReminderFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Reminder::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
