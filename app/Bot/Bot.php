048a240c5a01f72c249a2bef8fa5f103889674f6
adietyachristian <adietya.christian@ui.ac.id>
2021-04-02 23:16:45 +0700

    Handle common human error (number 1-13)

--- app/Bot/Bot.php
+++ app/Bot/Bot.php
@@ -146,6 +146,8 @@ class Bot
             $reply = static::doTranslate($message);
         } elseif (static::isAskingTime($message)) {
             $reply = static::tellTime($message);
+        } elseif (static::isSendingNumber($message)) {
+            static::sendMessage(__('bot.quick_reply_explanation'), $senderId);
         } elseif ($reply = static::showWikipedia($message)); elseif ($reply = static::showGoogleSearch($message)); else {
             $reply = __('bot.fallback_reply', compact('message'));
         }
