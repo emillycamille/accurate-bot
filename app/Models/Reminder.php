5092ef4bde156fe63f90c5f97b077168cf8cb9a7
adietyachristian <adietya.christian@ui.ac.id>
2021-04-04 20:42:14 +0700

    Add reminder cast

--- app/Models/Reminder.php
+++ app/Models/Reminder.php
@@ -17,4 +17,13 @@ class Reminder extends Model
     protected $fillable = [
         'psid', 'first_name', 'action', 'remind_at',
     ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'remind_at' => 'datetime',
+    ];
 }
